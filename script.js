9a686209bdad1f96c4619362ca1a05c5d7db85f9
Taiwo Joseph Afolabi <taiwojosephafolabi@gmail.com>
2023-01-09 22:17:51 +0000

    update to design

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+document.addEventListener("scroll", function () {
+  const nav_bar = document.querySelector(".nav_bar");
+  const navbarHeight = 100;
+  const distanceFromTop = Math.abs(
+    document.body.getBoundingClientRect().top
+  );
+
+  if (distanceFromTop >= navbarHeight) navbar.classList.add("fixed-top");
+  else navbar.classList.remove("fixed-top");
+});
+
+
+
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
