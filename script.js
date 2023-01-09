c04dedb41886b25418744a924efef030bc1ee266
Taiwo Joseph Afolabi <taiwojosephafolabi@gmail.com>
2023-01-09 20:37:35 +0000

    restore files

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8a0470ad5d82a18001cf0c90d7cb90400a8322991f807db672b9974dfdbf0034
-size 351
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementsByClassName("nav_bar").style.top = "0";
+  } else {
+    document.getElementsByClassName("nav_bar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
